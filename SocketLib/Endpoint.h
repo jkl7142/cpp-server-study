4c4b7c35ef481cdf464ad88767f1b2c37e146e0c
KimJiHyeok <jkl7142@naver.com>
2021-12-12 13:48:18 +0900

    Implement Endpoint Class

---
+++ SocketLib/Endpoint.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef _WIN32
+#include <WS2tcpip.h>
+#else
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#endif
+
+class Endpoint {
+public:
+	Endpoint();
+	Endpoint(const char* address, int port);
+	~Endpoint();
+
+	sockaddr_in m_ipv4Endpoint;
+
+	static Endpoint Any;
+	std::string ToString();
+};
