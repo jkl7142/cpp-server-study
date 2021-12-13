0d58e01c6fcd19cf09a7996d74a20594c10cf969
KimJiHyeok <jkl7142@naver.com>
2021-12-13 18:17:36 +0900

    Upload deadlock-example

---
+++ Thread/deadlock-example.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "CriticalSection.h"
+
+using namespace std;
+
+int a;
+CriticalSection a_mutex;
+int b;
+CriticalSection b_mutex;
+
+int main() {
+	// t1 스레드 시작
+	thread t1([]() {
+		while (1) {
+			CriticalSectionLock lock(a_mutex);
+			a++;
+			CriticalSectionLock lock2(b_mutex);
+			b++;
+			cout << "t1 done.\n";
+		}
+		});
+
+	// t2 스레드 시작
+	thread t2([]() {
+		while (1) {
+			CriticalSectionLock lock(b_mutex);
+			b++;
+			CriticalSectionLock lock2(a_mutex);
+			a++;
+			cout << "t2 done.\n";
+		}
+		});
+
+	t1.join();
+	t2.join();
+}
