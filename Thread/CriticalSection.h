a1d47f7347fe2c9e700cb512164fe1dae48202c2
KimJiHyeok <jkl7142@naver.com>
2021-12-13 18:17:18 +0900

    Implement CriticalSection Class

---
+++ Thread/CriticalSection.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class CriticalSection {
+	CRITICAL_SECTION m_critSec;
+public:
+	CriticalSection();
+	~CriticalSection();
+
+	void Lock();
+	void Unlock();
+};
+
+class CriticalSectionLock {
+	CriticalSection* m_pCritSec;
+public:
+	CriticalSectionLock(CriticalSection& critSec);
+	~CriticalSectionLock();
+};
