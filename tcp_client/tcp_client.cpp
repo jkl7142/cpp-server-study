5cbf6691e43c3c913478cbf079fb17b1f92d6e56
KimJiHyeok <jkl7142@naver.com>
2021-12-15 18:22:16 +0900

    Implement tcp_client

---
+++ tcp_client/tcp_client.cpp
@@ -0,0 +1,37 @@
+//
+// tcp_client.cpp
+// 
+// Defines the entry point for the console application.
+
+#include "stdafx.h"
+#include "../SocketLibrary/SocketLibrary.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main() {
+
+	try {
+		//SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		Socket s(SocketType::Tcp);
+		s.Bind(Endpoint::Any);
+		cout << "Connecting to server..." << "\n";
+		s.Connect(Endpoint("127.0.0.1", 5959));
+
+		cout << "Sending data..." << "\n";
+		const char* text = "hello";
+		s.Send(text, strlen(text) + 1);
+
+		std::this_thread::sleep_for(1s);
+
+		cout << "Closing socket..." << "\n";
+		s.Close();
+	}
+	catch (Exception& e) {
+		cout << "Exception! " << e.what() << endl;
+	}
+
+	return 0;
+}
