88ffb63eea091dfa84909ef1a21aa3396bd3290a
KimJiHyeok <jkl7142@naver.com>
2021-12-16 18:25:24 +0900

    Implement tcp_server

---
+++ tcp_server/tcp_server.cpp
@@ -0,0 +1,48 @@
+//
+// tcp_server.cpp
+//
+// Defines the entry point for the console application.
+
+#include "stdafx.h"
+#include "../SocketLibrary/SocketLibrary.h"
+
+using namespace std;
+
+int main() {
+
+	try {
+		Socket listenSocket(SocketType::Tcp);
+		listenSocket.Bind(Endpoint("0.0.0.0", 5959));
+		listenSocket.Listen();
+		cout << "Server started." << "\n";
+
+		Socket tcpConnection;
+		string ignore;
+		listenSocket.Accept(tcpConnection, ignore);
+
+		auto a = tcpConnection.GetPeerAddr().ToString();
+		cout << "Socket from " << a << " is accepted." << "\n";
+		while (true) {
+			string receivedData;
+			cout << "Receiving data..." << "\n";
+			int result = tcpConnection.Receive();
+
+			if (result == 0) {
+				cout << "Connection closed." << "\n";
+				break;
+			}
+			else if (result < 0) {
+				cout << "Connection lost: " << GetLastErrorAsString() << "\n";
+			}
+
+			cout << "Received: " << tcpConnection.m_receiveBuffer << "\n";
+		}
+
+		tcpConnection.Close();
+	}
+	catch (Exception& e) {
+		cout << "Exception! " << e.what() << endl;
+	}
+
+	return 0;
+}
