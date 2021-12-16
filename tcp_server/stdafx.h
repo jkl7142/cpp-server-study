80d724be61adefc38b0b502ba19c90696f7fda8f
KimJiHyeok <jkl7142@naver.com>
2021-12-16 18:25:10 +0900

    Upload precomfiled header

---
+++ tcp_server/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <sstream>
+#include <tchar.h>
+#include <cstdint>
+#include <WS2tcpip.h>
+#include <assert.h>
+#include <exception>
+#include <WinSock2.h>
